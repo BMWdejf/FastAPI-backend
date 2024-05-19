4f0ea061da8b880bd9e4c27f0733845d7353738d
BMWdejf <sablatura.david@gmail.com>
2024-05-19 12:30:00 +0200

    Change base.py for connecting with Neon.tech

--- app/db/base.py
+++ app/db/base.py
@@ -1,7 +1,26 @@
-from decouple import config
-from supabase import create_client, Client
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import declarative_base, sessionmaker
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-url = config("SUPABASE_URL")
-key = config("SUPABASE_KEY")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-supabase: Client = create_client(url, key)
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+metadata = MetaData()
+metadata.reflect(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+print("Existující tabulky v databázi:")
+for table in metadata.tables.values():
+    print(table.name)
+    print(table.columns)
