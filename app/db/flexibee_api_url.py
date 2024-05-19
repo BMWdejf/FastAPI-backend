6c71312c706e36200b977a0329b3d81293b55966
BMWdejf <sablatura.david@gmail.com>
2024-05-19 11:31:48 +0200

    Update flexibee_api_url.py and flexibee_data.py add asyncio function

--- app/db/flexibee_api_url.py
+++ app/db/flexibee_api_url.py
@@ -2,19 +2,56 @@ import requests
 import os
 from dotenv import load_dotenv
 from app.db.flexibee_data import get_data
+import functools
+import time
+from typing import Callable, Any
+import asyncio
 
 load_dotenv()
 
-def get_urls():
+def async_timed():
+    def wrapper(func: Callable) -> Callable:
+        @functools.wraps(func)
+        async def wrapped(*args, **kwargs) -> Any:
+            print(f'starting {func} with args {args} {kwargs}')
+            start = time.time()
+            try:
+                return await func(*args, **kwargs)
+            finally:
+                end = time.time()
+                total = end - start
+                print(f'finished {func} in {total:.4f} second(s)')
+
+        return wrapped
+
+    return wrapper
+
+@async_timed()
+async def get_urls():
     auth = (os.getenv("FLEXB_USER"), os.getenv("FLEXB_PASS"))
     response = requests.get("https://sas-technologi.flexibee.eu:5434/c/einteriors_s_r_o_/cenik.json?detail=custom:id&limit=1&order=id@D", auth=auth)
     data = response.json()['winstrom']['cenik']
 
     max_id = data[0]["id"]
+    #print(max_id)
 
-    for id_value in range(1, 10 + 1):
-        url = f"https://sas-technologi.flexibee.eu:5434/c/einteriors_s_r_o_/cenik/{id_value}.json?detail=custom:id,kod,nazev,exportNaEshop,prilohy(nazSoub,%20content,%20link,%20typK)&relations=prilohy"
-        get_data(url)
+    requests_data = []
+    last_index = 1
+    try:
+        for id_value in range(1, 10 + 1):
+            last_index = id_value
+            url = f"https://sas-technologi.flexibee.eu:5434/c/einteriors_s_r_o_/cenik/{id_value}.json?detail=custom:id,kod,nazev,exportNaEshop,prilohy(nazSoub,%20content,%20link,%20typK)&relations=prilohy"
+            requests_data.append(get_data(url))
+            #requests_data.append(asyncio.to_thread(get_data, url))
+
+        #print(requests_data)
+        results = await asyncio.gather(*requests_data)
+
+        for row in results:
+            print(row)
+    except Exception as e:
+        print(e)
+        print(last_index)
 
 if __name__ == "__main__":
-    get_urls()
+    asyncio.run(get_urls())
--- app/db/flexibee_data.py
+++ app/db/flexibee_data.py
@@ -1,12 +1,14 @@
 import requests
 import os
 from dotenv import load_dotenv
-
+import asyncio
 load_dotenv()
 
-def get_data(url):
+async def get_data(url):
     auth = (os.getenv("FLEXB_USER"), os.getenv("FLEXB_PASS"))
-    response = requests.get(url, auth=auth)
+    response = await asyncio.to_thread(requests.get, url, auth=auth)
+    #response = requests.get(url, auth=auth)
     data = response.json()['winstrom']['cenik']
     #TODO: Write for cycle for import data to database
-    print(data)
+    #print(data)
+    return data
