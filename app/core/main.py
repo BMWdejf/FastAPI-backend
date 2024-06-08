66cc9d4c7a2ba37b9c6dd7e05d6562950b75598b
BMWdejf <sablatura.david@gmail.com>
2024-06-08 12:11:29 +0200

    Add /token for auth /import_products

--- app/core/main.py
+++ app/core/main.py
@@ -1,14 +1,22 @@
-from fastapi import FastAPI, Depends
-from app.utils.authentication import verify_token
-from app.db.flexibee_api_url import get_data as import_products_function
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from app.db.flexibee_api_url import import_products
+from app.utils.auth import get_current_user, authenticate_user, create_access_token, Token, db, timedelta, ACCESS_TOKEN_EXPIRE_MINUTES
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello from FastAPI!"}
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Incorrect username or password", headers={"WWW-Authenticate": "Bearer"})
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
 
 @app.get("/import_products")
-async def import_products_endpoint(valid_token: bool = Depends(verify_token)):
-    await import_products_function()
+async def import_products_endpoint(user: str = Depends(get_current_user)):
+    await import_products()
     return {"message": "Products imported successfully"}
